68a9d4c106c116494853a498134d67957a4a1868
babakfp <babak.bxf@gmail.com>
2021-11-04 10:13:46 +0330

    Added goup btn

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,13 +17,37 @@
 		'/faq': Page,
 	}
 	$: layout = layouts[$page.path] ?? Default
+
+	let hidden = true
+
+	// Prevents navbar to show/hide when clicking in docs sidebar
+	let hash_changed = false
+	function handle_hashchange() {
+		hash_changed = true
+	}
+	let last_scroll = 0
+	function handle_scroll() {
+		const scroll = window.pageYOffset
+		if (!hash_changed) {
+			hidden = ((scroll > 50) && (scroll > last_scroll))
+		}
+		last_scroll = scroll
+		hash_changed = false
+	}
 </script>
+
+<svelte:window on:hashchange={handle_hashchange} on:scroll={handle_scroll} />
 
 <LoadingBar />
 
 <svelte:component this={layout}>
 	<slot />
 </svelte:component>
+
+<a class:goup--hidden={hidden} class="z-50 fixed bottom-4 right-4 flex items-center justify-center w-15 h-15 rounded-full bg-darkest text-white border-2 border-white border-opacity-5 transition duration-300 ease-in-out"
+	href="#body">
+	UP
+</a>
 
 <style lang="postcss">
 	:global(#body #svelte .loading-bar .loading-bar__train:before),
@@ -32,5 +56,8 @@
 	}
 	:global(.loading-bar) {
 		@apply h-1 md:h-0.5;
+	}
+	:global(.goup--hidden) {
+		@apply transform translate-y-20;
 	}
 </style>
