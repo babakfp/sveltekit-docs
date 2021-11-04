3cf51407b1d655f377936221a6517cf51e630811
babakfp <babak.bxf@gmail.com>
2021-11-04 09:21:29 +0330

    maybe fixed sidebar and mmenu default open

--- src/stores/header.js
+++ src/stores/header.js
@@ -1,8 +1,8 @@
 import { writable, readable } from 'svelte/store'
 import { browser } from '$app/env'
 
-export const isMMenuOpen = writable((browser && (window.innerWidth >= 768)) ? true : false)
-export const isSideMenuOpen = writable((browser && (window.innerWidth >= 1024)) ? true : false)
+export const isMMenuOpen = writable(browser && window.innerWidth >= 768 ? true : false)
+export const isSideMenuOpen = writable(browser && window.innerWidth >= 1024 ? true : false)
 
 export const toggleMMenu =_=> {
 	if (!browser) return
