def1b53a2e83c903d58e6020cbcd6f821f0e1824
babakfp <babak.bxf@gmail.com>
2021-11-04 09:24:28 +0330

    added prin sidebar link target _blank

--- src/lib/sidemenu/PrimItem.svelte
+++ src/lib/sidemenu/PrimItem.svelte
@@ -8,6 +8,7 @@
 	<a
 		class="py-2 px-5 flex gap-3 items-center font-semibold text-sm text-white-70 transition-all duration-200 ease-in-out hover:text-white focus:text-white lg:px-6"
 		{href}
+		target="_blank"
 	>
 		<img class="w-7 h-7" src="/svg/prim/{icon}.svg" alt />
 		<span>{title}</span>
