683e3cbf89c43c65f3251b412b1446a39a0f3195
babakfp <babak.bxf@gmail.com>
2021-11-04 08:44:13 +0330

    Added loading bar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,8 @@
 	import { page } from '$app/stores';
 	import '../app.css'
 
+	import LoadingBar from ':lib/LoadingBar.svelte'
+
 	// Layouts
 	import Default from ':layouts/Default.svelte'
 	import Home from ':layouts/Home.svelte'
@@ -17,6 +19,18 @@
 	$: layout = layouts[$page.path] ?? Default
 </script>
 
+<LoadingBar />
+
 <svelte:component this={layout}>
 	<slot />
 </svelte:component>
+
+<style lang="postcss">
+	:global(#body #svelte .loading-bar .loading-bar__train:before),
+	:global(#body #svelte .loading-bar .loading-bar__train:after) {
+		@apply bg-brand;
+	}
+	:global(.loading-bar) {
+		height: 2px !important;
+	}
+</style>
