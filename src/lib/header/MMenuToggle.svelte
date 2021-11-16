66b5a6b4fe907d656eaf301f2bfdc9b01cf18cfc
babakfp <babak.bxf@gmail.com>
2021-11-16 22:16:44 +0330

    Fixed MMenu Toggle

--- src/lib/header/MMenuToggle.svelte
+++ src/lib/header/MMenuToggle.svelte
@@ -2,7 +2,7 @@
 	import { isMMenuOpen, toggleMMenu } from ':stores/header.js'
 </script>
 
-<button class="mmenu-toggle group -ml-2 px-4 md:hidden" on:click={toggleMMenu}>
+<button class="mmenu-toggle group -ml-4 px-4 md:hidden" on:click={toggleMMenu}>
 	<div class="w-6 grid {!$isMMenuOpen && 'gap-1.5'} transition-all duration-200">
 		<div class="h-0.5 {$isMMenuOpen && '-mb-px rotate-45'} bg-white bg-opacity-70 rounded-full transition-all duration-200 transform group-hover:bg-opacity-100 group-focus:bg-opacity-100" />
 		<div class="h-0.5 {$isMMenuOpen && '-mt-px -rotate-45'} bg-white bg-opacity-70 rounded-full transition-all duration-200 transform group-hover:bg-opacity-100 group-focus:bg-opacity-100" />
